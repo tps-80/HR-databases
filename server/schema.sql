cccaec5705b19e608e25a82135098272c473126c
tps-80 <thomaspsorensen@gmail.com>
2015-10-30 19:25:28 -0700

    Completed schema.sql for current schema.

--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,27 @@
-CREATE DATABASE chat;
+CREATE DATABASE chat1;
 
-USE chat;
+USE chat1;
+
+
+CREATE TABLE users (
+  user_id integer auto_increment primary key,
+  name varchar(50)
+);
+
+CREATE TABLE rooms (
+  rooms_id integer auto_increment primary key,
+  room varchar(50)
+);
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message_id integer auto_increment primary key,
+  msgText varchar(500),
+  user integer,
+  room integer,
+  foreign key (user) references users (user_id),
+  foreign key (room) references rooms (rooms_id)
 );
-
-/* Create other tables and define schemas for them here! */
 
 
 
